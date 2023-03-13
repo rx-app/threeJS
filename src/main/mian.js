be60896d80d5c7e5ed56ca1151ed546eb9f514ec
dingming <lol>
2023-03-13 22:22:46 +0800

    12

--- src/main/mian.js
+++ src/main/mian.js
@@ -42,3 +42,11 @@ function render(){
 }
 
 render()
+
+
+window.addEventListener('resize',()=>{
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    renderer.setPixelRatio(window.devicePixelRatio)
+})
