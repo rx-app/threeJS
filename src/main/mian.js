15c633a1c06cd407279246dd45535a39265d6209
dingming <lol>
2023-03-13 10:39:50 +0800

    4

--- src/main/mian.js
+++ src/main/mian.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+// 轨道控制器
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 
@@ -15,5 +17,14 @@ scene.add( cube );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+//渲染
+// renderer.render(scene,camera)
 
-renderer.render(scene,camera)
+const controls = new OrbitControls(camera,renderer.domElement)
+
+function render(){
+    renderer.render(scene,camera)
+    requestAnimationFrame(render)
+}
+
+render()
